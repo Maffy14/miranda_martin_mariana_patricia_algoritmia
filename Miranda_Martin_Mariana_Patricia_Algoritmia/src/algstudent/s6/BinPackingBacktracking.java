ea17b26f7f06b15bca127691ded955376e11354e
Mariana Patricia Miranda Martín <UO307423@uniovi.es>
2026-03-26 19:30:28 +0100

    sessions 0 (done), 1.1 (done), 1.2 (done), 2 (done), 3 (done), 4 (done), 5 and 6

---
+++ Miranda_Martin_Mariana_Patricia_Algoritmia/src/algstudent/s6/BinPackingBacktracking.java
@@ -0,0 +1,162 @@
+package algstudent.s6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// ordenar lista de elementos de menor a mayor
+/*
+
+	The Bin Packing Problem is considered in its classic form: 
+	
+	Given a finite set of objects, each with a positive size, and an unlimited number of identical 
+	containers (bins), each with a fixed maximum capacity, the goal is to 
+		determine the object-to-container assignment that minimizes the total number of containers used, 
+		while always satisfying the capacity constraint. 
+	
+	This problem is recognized as NP-hard, meaning that no polynomial-time algorithms are known that 
+	guarantee obtaining the optimal solution for general instances. 
+	
+	The objective of this exercise is to design and implement an exact backtracking algorithm (with 
+	pruning?) capable of exploring the solution space and determining the minimum number of 
+	containers needed to store all the objects of a given instance. 
+	
+	The solution must, at a minimum: 
+
+	1. Recursively explore all possible object-to-container assignments. 
+	2. Maintain and continuously update the best solution found.  
+	3. Maintain a state representation that allows for efficient forward and backward operations. 
+	
+	FORMAL SPECIFICATION OF THE PROBLEM 
+	
+	Let: 
+		𝑆={𝑠1,𝑠2,…,𝑠𝑛} be a set of objects, where each object Si has a positive integer size. 
+		𝐶∈ℕ  is the fixed integer capacity of each container. 
+		Sets 𝐵1,𝐵2,…,𝐵𝑘 represent the containers used, where each 𝐵𝑗 ⊆ 𝑆. 
+	
+	The goal is to find the minimum value k such as: 
+	
+		No container exceeds the maximum permitted capacity: 
+				∑    𝑠≤𝐶 ∀𝑗 ∈ {1,…,𝑘} 
+				𝑠∈𝐵𝑗
+		
+		The union of all containers contains exactly the set of all objects, and the containers form a 
+		partition of the set of objects: 
+			k
+			⋃𝐵𝑗 = 𝑆, 𝐵𝑖∩𝐵𝑗 = ∅ if 𝑖 ≠ 𝑗. 
+			𝑗=1
+
+	INPUT AND OUTPUT OF THE PROGRAM 
+	
+	Input 
+	
+	A text file with the following format: 
+	
+		First line: a positive integer C, representing the capacity of the containers. 
+		Second line: a sequence of positive integers representing the sizes of the objects. 
+	
+	For example, in test00.txt the following input is provided: 
+	
+		10 
+		7 6 5 5 4 3 2 
+
+	Output 
+	
+	The program must produce: 
+		1. Minimum number of containers needed to store all objects. 
+		2. Final distribution of the objects in each container, enumerated from 1 to 
+		 	the total amount of used containers 
+	
+	For example, considering the previous input a solution would be the following one: 
+		List of bins and their objects: 
+		
+			Bin 1: 3 7  
+			Bin 2: 4 6  
+			Bin 3: 5 5  
+			Bin 4: 2  
+		
+		The minimum number of bins is 4. 
+	
+	3. Optionally, the number of recursive calls made can also be printed
+
+ */
+public class BinPackingBacktracking {
+	
+	private int capacity;
+	private List<Integer> sizesObjects;
+	private List< List<Integer> > bins;
+	private List<Integer> capacityBins; 
+	List<Integer> currentBin;
+	int currentCapacityBin;
+	private int minBins;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// Constructor
+	public BinPackingBacktracking(int capacity, List<Integer> sizes) {
+		this.capacity = capacity;
+		this.sizesObjects = sizes;
+		bins = new ArrayList<>();
+		capacityBins = new ArrayList<>();
+		currentBin = new ArrayList<>();
+	}
+
+	
+
+	/**
+	 * Prints the input data of the problem
+	 */
+	public void printData() {
+		StringBuilder inputData = new StringBuilder();
+		
+		inputData.append("Size objects-to-be-stored in bins ").append(capacity);
+		
+	}
+
+	public void backtracking() {
+		
+		
+		// Base check -> We have found the solution
+		if(currentCapacityBin > this.capacity) { // We have processed all objects
+			// Let's find the solution in 
+		
+			return;
+		}
+		
+		// we explore all possible choices
+		bins.add(currentBin);
+		
+		for (int sizeObject = 0; sizeObject < sizesObjects.size(); sizeObject++) {
+			
+			if (sizeObject <= currentCapacityBin) {
+				// We choose a path
+				currentBin.add(sizeObject);
+				currentCapacityBin += sizeObject;
+				
+				// We recursively explore the current path
+				backtracking();
+				
+				// We undo last path
+				currentBin.remove(currentBin.size() - 1); // remove last element
+				currentCapacityBin -= sizeObject;
+			} 
+		}
+		
+		
+	}
+
+	public void printSolution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @return minimum number of containers needed to store all objects. 
+	 */
+	public int getBinsNeededSolution() {
+		return minBins;
+	}
+
+}
---
+++ Miranda_Martin_Mariana_Patricia_Algoritmia/src/algstudent/s6/BinPackingBacktrackingTests.java
@@ -0,0 +1,139 @@
+package algstudent.s6;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class BinPackingBacktrackingTests {
+	private int capacity;
+	private List<Integer> sizes;
+	
+	@Test
+	public void test00() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test00.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(4, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test01() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test01.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(1, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test02() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test02.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(8, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test03() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test03.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(2, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test04() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test04.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(10, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test05() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test05.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(14, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test06() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test06.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(7, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test07() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test07.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(8, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test08() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test08.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(16, problem.getBinsNeededSolution());
+	}
+	
+	@Test
+	public void test09() {
+		loadData("src/main/java/labs/en/_26/algteacher/s6/test09.txt");		
+		BinPackingBacktracking problem = new BinPackingBacktracking(this.capacity, this.sizes);
+		problem.printData();
+		problem.backtracking();
+		problem.printSolution();
+		assertEquals(33, problem.getBinsNeededSolution());
+	}
+
+	private void loadData(String file) {
+		this.sizes = new ArrayList<Integer>();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			this.capacity = Integer.valueOf(reader.readLine());		
+			for (String s : reader.readLine().split(" ")) {
+				this.sizes.add(Integer.valueOf(s));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
